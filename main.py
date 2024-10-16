319cb2f6b04129891e59314397279221c15ff454
achervets <achervets@gmail.com>
2024-10-16 16:15:59 -0700

    sorted game objects into containers

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -13,9 +18,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+            
+        for field in updatable:
+            field.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for field in drawable:
+            field.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 fps
